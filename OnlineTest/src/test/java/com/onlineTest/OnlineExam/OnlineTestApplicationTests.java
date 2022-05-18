51380a1f18f623b0274724936bcc4888636bf9d6
vrushabh wani <105492738+vrushabhWani@users.noreply.github.com>
2022-05-18 10:30:55 +0530

    send
    send

---
+++ OnlineTest/src/test/java/com/onlineTest/OnlineExam/OnlineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineTest.OnlineExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
